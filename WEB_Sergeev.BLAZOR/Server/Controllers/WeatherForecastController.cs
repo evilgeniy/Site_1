a8ef75a9d8920d6a5b821a818d88e5259b99725e
evilgeniy <62379284+evilgeniy@users.noreply.github.com>
2022-03-13 14:12:26 +0300

    Add files via upload

---
+++ WEB_Sergeev.BLAZOR/Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WEB_953506_KONDRAHOV.BLAZOR.Shared;
+using WEB_953506_KONDRASHOV.Data;
+using WEB_953506_KONDRASHOV.Entities;
+
+namespace WEB_953506_KONDRAHOV.BLAZOR.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private ApplicationDbContext _context;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, ApplicationDbContext context)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Engine>>> GetEngines(int? group)
+        {
+            var engines = _context.Engines.Where(d => !group.HasValue || d.ClassId == group.Value);
+
+            return await engines.ToListAsync();
+        }
+    }
+}

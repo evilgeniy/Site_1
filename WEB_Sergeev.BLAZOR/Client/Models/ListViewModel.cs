32bf6c10c8e3cee82d4332583ab6e41543732879
evilgeniy <62379284+evilgeniy@users.noreply.github.com>
2022-03-13 14:25:36 +0300

    Add files via upload

---
+++ WEB_Sergeev.BLAZOR/Client/Models/ListViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace WEB_953506_KONDRAHOV.BLAZOR.Client.Models
+{
+    public class ListViewModel
+    {
+        [JsonPropertyName("engineId")]
+        public int EngineId { get; set; }
+        [JsonPropertyName("engineName")]
+        public string EngineName { get; set; } 
+    }
+
+    public class DetailsViewModel
+    {
+        [JsonPropertyName("engineName")]
+        public string EngineName { get; set; }
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+        [JsonPropertyName("price")]
+        public int Price { get; set; }
+        [JsonPropertyName("image")]
+        public string Image { get; set; }
+    }
+}
